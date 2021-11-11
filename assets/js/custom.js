0104e52f17546f212d3901b901f4e1153376aff8
Sarthak Akre <sarthakakre27@gmail.com>
2021-11-11 11:57:25 +0530

    comments added in js files, footer text decor changed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,19 @@
-let flag1 = 0;
-const form = document.getElementById("form");
+let flag1 = 0; // flag for form validation check
+const form = document.getElementById("form"); // get the form on the page
 if (form) {
+  //get all the fields in the form which needs validation
   const title = document.getElementById("title");
   const author = document.getElementById("author");
   const image = document.getElementById("image");
   const imagebuy = document.getElementById("image-buy");
   const price = document.getElementById("price");
-  const description = document.getElementById("description");
 
+  //add a event listener on submit
   form.addEventListener("submit", (e) => {
     flag1 = 0;
-    e.preventDefault();
-    checkInputs();
-    if(flag1 === 0)
+    e.preventDefault(); // prevent submission
+    checkInputs(); // check for fields
+    if(flag1 === 0) // if fields safe then submit
     {
       document.getElementById("form").submit();
     }
@@ -25,60 +26,60 @@ if (form) {
     const imageValue = image.value.trim();
     const imagebuyValue = imagebuy.value.trim();
     const priceValue = price.value.trim();
-    // const descriptionValue = description.value.trim();
 
+    // if value empty then set error classes on the respective html
     if (titleValue === "") {
       setErrorFor(title, "Title cannot be blank");
       flag1 = 1;
-    } else {
+    } else { // else set success classes on the form
       setSuccessFor(title);
     }
 
+    // if value empty then set error classes on the respective html
     if (authorValue === "") {
       setErrorFor(author, "Author cannot be blank");
       flag1 = 1;
-    } else {
+    } else { // else set success classes on the form
       setSuccessFor(author);
     }
 
+    // if value empty then set error classes on the respective html
     if (imageValue === "") {
       setErrorFor(image, "Enter url");
       flag1 = 1;
     } else if (!isimage(imageValue)) {
-      setErrorFor(image, "Url not valid");
+      setErrorFor(image, "Url not valid"); // check for uri
       flag1 = 1;
-    } else {
+    } else { // else set success classes on the form
       setSuccessFor(image);
     }
 
+    // if value empty then set error classes on the respective html
     if (imagebuyValue === "") {
       setErrorFor(imagebuy, "Enter url");
       flag1 = 1;
     } else if (!isimagebuy(imagebuyValue)) {
-      setErrorFor(imagebuy, "Url not valid");
+      setErrorFor(imagebuy, "Url not valid"); // check for uri
       flag1 = 1;
-    } else {
+    } else { // else set success classes on the form
       setSuccessFor(imagebuy);
     }
-
+    
+    // if value empty then set error classes on the respective html
     if (String(priceValue) === "") {
       setErrorFor(price, "Enter valid price");
       flag1 = 1;
     }
+    // if value empty then set error classes on the respective html
     if (Number(priceValue) <= 0) {
-      setErrorFor(price, "Enter valid price");
+      setErrorFor(price, "Enter valid price");// check for negative price
       flag1 = 1;
-    } else {
+    } else { // else set success classes on the form
       setSuccessFor(price);
     }
-
-    // if(descriptionValue === '') {
-    // 	setErrorFor(priceValue, 'Enter a Excerpt of the Book');
-    // } else{
-    // 	setSuccessFor(priceValue);
-    // }
   }
 
+  //function to add error classes for respective html
   function setErrorFor(input, message) {
     const formControl = input.parentElement;
     const small = formControl.querySelector("small");
@@ -86,11 +87,13 @@ if (form) {
     small.innerText = message;
   }
 
+  //function to add success classes for respective html
   function setSuccessFor(input) {
     const formControl = input.parentElement;
     formControl.className = "form-control success";
   }
 
+  //function to check uri
   function isimage(image) {
     if (image.substr(0, 8) === "https://") {
       return true;
@@ -98,7 +101,7 @@ if (form) {
       return false;
     }
   }
-
+  //function to check uri
   function isimagebuy(imagebuy) {
     if (imagebuy.substr(0, 8) === "https://") {
       return true;
--- assets/js/custom1.js
+++ assets/js/custom1.js
@@ -1,14 +1,16 @@
-let flag = 0;
-const form2 = document.getElementById("form2");
+let flag = 0; // flag for form validation check
+const form2 = document.getElementById("form2"); // get the form on the book-show page
 if (form2) {
+	//get all the fields in the form which needs validation
 	const inputmessage=document.getElementById("inputmessage");
 	const inputname=document.getElementById("inputname");
   
+	//add a event listener on submit
 	form2.addEventListener("submit", (e) => {
       flag = 0;
-	  e.preventDefault();
-	  checkInputs2();
-      if(flag == 0)
+	  e.preventDefault(); // prevent submission
+	  checkInputs2(); // check for fields
+      if(flag == 0) // if fields safe then submit
       {
         document.getElementById("form2").submit();
       }
@@ -19,47 +21,52 @@ if (form2) {
 	  const inputmessageValue=inputmessage.value.trim();
 	  const inputnameValue=inputname.value.trim();
   
+	  // if value empty then set error classes on the respective html
 	  if (inputnameValue === "") {
 		  setErrorFor2(inputname, "Enter your username");
           flag = 1;
-		} else {
+		} else { // else set success classes on the form
 		  setSuccessFor2(inputname);
 	  }
   
+	  // if value empty then set error classes on the respective html
 	  if (inputmessageValue === "") {
 		  setErrorFor2(inputmessage, "Review is not filled");
           flag = 1;
-	  } else if(inputmessageValue.length>500){
-			setErrorFor2(inputmessage, "Review cannot exceed more than 500 characters");
-            flag = 1;
-	  } else{
-		setSuccessFor2(inputmessage);
-	  }
-	function setErrorFor2(input, message) {
-	  const formControl = input.parentElement;
-	  const small = formControl.querySelector("small");
-	  formControl.className = "form-control error";
-	  small.innerText = message;
-	}
-  
-	function setSuccessFor2(input) {
-	  const formControl = input.parentElement;
-	  formControl.className = "form-control success";
-	}
+	  	} else if(inputmessageValue.length > 500){ // check if text length is > 500
+				setErrorFor2(inputmessage, "Review cannot exceed more than 500 characters");
+      	      flag = 1;
+	  	} else{ // else set success classes on the form
+			setSuccessFor2(inputmessage);
+	  	}
+		//function to add error classes for respective html
+		function setErrorFor2(input, message) {
+		  const formControl = input.parentElement;
+		  const small = formControl.querySelector("small");
+		  formControl.className = "form-control error";
+		  small.innerText = message;
+		}
+		//function to add success classes for respective html
+		function setSuccessFor2(input) {
+		  const formControl = input.parentElement;
+		  formControl.className = "form-control success";
+		}
 
 	}
   }
 
 //Star Rating functionality//
+// using Jquery for the star rating
 $(document).ready(function(){
-    // Check Radio-box
+    // clear Check Radio-box
     $(".rating input:radio").attr("checked", false);
 
+	// on a click ti a star add checked class on parent
     $('.rating input').click(function () {
         $(".rating span").removeClass('checked');
         $(this).parent().addClass('checked');
     });
-
+	//chenge the value of the input to checked stars
     $('input:radio').change(
       function(){
         var userRating = this.value;
@@ -68,11 +75,13 @@ $(document).ready(function(){
 
 var starWidth = 40;
 
+//set the width of each star
 $.fn.stars = function() {
   return $(this).each(function() {
     $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat($(this).html())))) * starWidth));
   });
 }
+// on document loaded show stars
 $(document).ready(function() {
   $('span.stars').stars();
 });
